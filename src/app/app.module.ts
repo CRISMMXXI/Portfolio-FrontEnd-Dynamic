a0a45ac52e48d8892073c89113df0ef9d860ff5c
CRISMMXXI <cris.life121@gmail.com>
2022-05-24 12:40:08 -0300

    mi primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SobreMiComponent,
+    EducacionComponent,
+    HabilidadesComponent,
+    ProyectosComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/sobre-mi/sobre-mi.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/services/portfolio.service';
+
+
+@Component({
+  selector: 'app-sobre-mi',
+  templateUrl: './sobre-mi.component.html',
+  styleUrls: ['./sobre-mi.component.css']
+})
+export class SobreMiComponent implements OnInit {
+  miPortfolio: any;
+
+  constructor( private datosPortfolio: PortfolioService ) { }
+
+  ngOnInit(): void {
+this.datosPortfolio.obtenerDatos()
+.subscribe( data => this.miPortfolio = data);
+  }
+
+}
